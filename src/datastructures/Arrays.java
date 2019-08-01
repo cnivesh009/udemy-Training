d9dd8c2fda6b5fac0d3098c478ff7c36ae82bfc6
cnivesh009 <cnivesh@hotmail.com>
2019-08-01 19:34:39 +0530

    initial add to repository

---
+++ src/datastructures/Arrays.java
@@ -0,0 +1,17 @@
+/*
+ * @author:        Nivesh Chandola
+ * Date:           01.08.2019
+ * Program:        Data Structures - Arrays
+ * Last Edited:    01.08.2019
+ */
+
+package datastructures;
+
+public class Arrays {
+    public static void main(String[] args) {
+		String[] alphabet = {"a", "b", "c", "d", "e", "f"};
+		for(int i = 0; i < alphabet.length; i++){
+			System.out.println(alphabet[i]);
+		}
+    }
+}
