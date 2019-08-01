f4bcda2919c549e81fad5dda3f05d3dc032b9cc3
cnivesh009 <cnivesh@hotmail.com>
2019-08-01 20:02:59 +0530

    updated Arrays.java

--- src/datastructures/Arrays.java
+++ src/datastructures/Arrays.java
@@ -9,9 +9,22 @@ package datastructures;
 
 public class Arrays {
     public static void main(String[] args) {
+		String str = " ";
 		String[] alphabet = {"a", "b", "c", "d", "e", "f"};
-		for(int i = 0; i < alphabet.length; i++){
+		System.out.println("String[] alphabet = {\"a\", \"b\", \"c\", \"d\", \"e\", \"f\"};");
+		System.out.println("The old fashioned way of using for loop:");
+		System.out.println("for (int i = 0; i < 5; i++){	// when size is known");
+		System.out.println("  System.out.println(alphabet[i]);\n}");
+		for (int i = 0; i < 5; i++){
 			System.out.println(alphabet[i]);
 		}
+		
+		System.out.println("An alternative of using for loop:");
+		System.out.println("for(int i = 0; i < alphabet.length; i++){	// when size is not known");
+		System.out.println("   System.out.print(alphabet[i]" + " + " + "\" \"" + ");" + "\n}");
+		for(int i = 0; i < alphabet.length; i++){
+			System.out.print(alphabet[i] + " ");
+		}
+		System.out.println();
     }
 }
