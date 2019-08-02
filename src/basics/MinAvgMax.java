a5c8495a0818f760389a46a4e860128995e45ba8
Nivesh Chandola <cnivesh@hotmail.com>
2019-08-02 11:39:43 +0530

    Update MinAvgMax.java

--- src/basics/MinAvgMax.java
+++ src/basics/MinAvgMax.java
@@ -18,7 +18,7 @@ public class MinAvgMax{
 	System.out.println("*****Displays Mini Avg and Max Values*****");
         System.out.println("Minimum value: " + min);
 	System.out.println("Average value: " + avg);
-	System.out.println("Maximum value: " + max);
+	System.out.println("Maximum value: " + max);a
     }
     
     public static int Minimum(int[] arr){
