175a9d3b831d0b0873c514b0f77b5fe759dd4d2b
Nivesh Chandola <cnivesh@hotmail.com>
2019-08-10 23:54:37 +0530

    Update Demo.java

--- src/oop/Demo.java
+++ src/oop/Demo.java
@@ -1,8 +1,8 @@
 /*
- * @author        Nivesh Chandola
- * Date           30/07/2019
- * Program        Revision
- * Last Edited    10/08/2019
+ * @author      : Nivesh Chandola
+ * Created on	: 30/07/2019
+ * Program      : Revision
+ * Last Edited  : 10/08/2019
  */
 
 package oop;
