8118e05cb4cbc057402e3da18ba384ac607006e1
Nivesh Chandola <cnivesh@hotmail.com>
2019-08-10 23:46:58 +0530

    Update Demo.java

--- src/exceptions/Demo.java
+++ src/exceptions/Demo.java
@@ -1,8 +1,8 @@
 /*
- * @author        Nivesh Chandola
- * Date           31/07/2019
- * Program        Exception
- * Last Edited    10/08/2019
+ * @author      : Nivesh Chandola
+ * Created on	: 31/07/2019
+ * Program      : Exception
+ * Last Edited  : 10/08/2019
  */
 
 package exceptions;
