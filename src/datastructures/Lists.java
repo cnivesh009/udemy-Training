8af818740a8bb88e9b68cdb52ca9b99b96efd32a
Nivesh Chandola <cnivesh@hotmail.com>
2019-08-10 23:38:48 +0530

    Update Lists.java

--- src/datastructures/Lists.java
+++ src/datastructures/Lists.java
@@ -1,8 +1,8 @@
 /*
- * @author	Nivesh Chandola
- * Created on	10/08/2019, 6:08:38 PM           
- * Program      Lists   
- * Last Edited  10/08/2019   
+ * @author	: Nivesh Chandola
+ * Created on	: 10/08/2019, 6:08:38 PM           
+ * Program      : Lists   
+ * Last Edited  : 10/08/2019   
  */
 
 package datastructures;
