984501a917e4897e864ef232f2e45e31bce65411
Nivesh Chandola <cnivesh@hotmail.com>
2019-08-10 23:43:56 +0530

    Update Sum_n_Numbers.java

--- src/basics/Sum_n_Numbers.java
+++ src/basics/Sum_n_Numbers.java
@@ -1,8 +1,8 @@
 /*
- * @author        Nivesh Chandola
- * Date           22/07/2019
- * Program        Sum of 1 to n Numbers
- * Last Edited    10/08/2019
+ * @author	: Nivesh Chandola
+ * Created on	: 22/07/2019
+ * Program      : Sum of 1 to n Numbers
+ * Last Edited  : 10/08/2019
  */
 
 package basics;
