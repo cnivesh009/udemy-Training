e7f389154f3277d0824aae6e0690f2c1547a5f7b
cnivesh009 <cnivesh@hotmail.com>
2019-08-11 14:12:12 +0530

    initial add to repo

---
+++ src/datastructures/NewMain.java
@@ -0,0 +1,13 @@
+/*
+ *  @author		: Nivesh Chandola
+ *  Created on11/08/2019, 2:06:08 PM
+ *  Program		: 	
+ *  Last Edited	: 
+ */
+
+package datastructures;
+
+public class NewMain {
+    public static void main(String[] args) {   
+    }
+}
