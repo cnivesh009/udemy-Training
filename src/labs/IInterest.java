53f86686c95d58680ac83b64c64b7df81e81b34c
cnivesh009 <cnivesh@hotmail.com>
2019-08-25 23:53:03 +0530

    update IInterest.java

--- src/labs/IInterest.java
+++ src/labs/IInterest.java
@@ -1,14 +1,13 @@
-/*
- * @author      : Nivesh Chandola
- * created on   : 30/07/2019
- * Program      : Interest
- * Last Edited  : 24/08/2019
+/**
+ *    Document   : IInterest
+ *    Created on : 30/07/2019, 10:02:18 PM
+ *    Author     : Nivesh-GC
  */
-
 package labs;
 
-public interface IInterest{
-    public double rate = 4.5;
-    
-    public void accrue();   //	this is abstract method, can't have body
+public interface IInterest {
+
+	public double rate = 4.5;
+
+	public void accrue();   //	this is abstract method, can't have body
 }
