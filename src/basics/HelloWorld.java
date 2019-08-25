22a332432b14eb06ade5c535b6d9b403ed0efd70
cnivesh009 <cnivesh@hotmail.com>
2019-08-25 22:32:21 +0530

    initial add to repository

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -1,15 +1,14 @@
-/*
- *	Document   : HelloWorld
- *  Created on : 25/08/2019, 9:30:45 PM
- *  Author     : Nivesh-GC
- */
+/**
+ *		Document   : HelloWorld
+ *		Created on : 25/08/2019, 10:30:44 PM
+ *		Author     : Nivesh-GC
+ * */
 package basics;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Adding code from GITHUB");
 	}
 
 }
